20609a97c220b09062b0d8241928c411e39dbf56
nisha <nisharawat2702@gmail.com>
2023-07-10 16:19:01 +0530

    1

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'root',
-  database: 'dac2023',
+  password: 'manager',
+  database: 'dac',
   port: '7500'
 })
 
